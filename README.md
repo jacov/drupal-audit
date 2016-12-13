24e345be79f53acdf846d71495b195a347bc1e90
jacov <jacob@imunchie.com>
2016-12-13 10:49:30 -0500

    b"h v1

---
+++ README.md
@@ -0,0 +1,26 @@
+# Drupal Audit
+
+This tool produces a quick and dirty report of some drush commands.
+
+This should work for Drupal 7 & 8*
+
+* d8 may have some errors still...
+
+
+
+
+## Usage
+### With Drush Aliases
+`
+./drupal-audit.sh @wabee2.prod
+`
+
+### Without Drush Aliases 
+`
+cd <drupal_path>
+./drupal-audit.sh
+`
+* Note: if you are running without Drush Aliases, make sure to clean up output dir, or set it to a different location
+
+
+![Alt](https://s3.amazonaws.com/public-jacob/misc/drupal_logo.png "Drupal")
---
+++ drupal-audit.sh
@@ -0,0 +1,188 @@
+#!/bin/bash
+
+export DRUSH_ALIAS=$1
+
+export ENVIRONMENT=`echo "$1" | sed -e 's/@//g'`
+
+export TODAY=`date "+%Y-%m-%d"`
+export OUTDIR="output"
+export REPORT="${OUTDIR}/${TODAY}_${ENVIRONMENT}.html"
+
+
+# MAIN
+
+# create output dir
+mkdir -p $OUTDIR
+
+
+# initialize report & compose head
+echo "<html>
+<head>
+	<title>Drupal Audit : $ENVIRONMENT : $TODAY</title>
+<style>
+pre {
+	display: block;
+        padding: 0.5em 1em;
+        border: 1px solid #bebab0;
+	background: #faf8f0;
+	font-family: "Courier 10 Pitch", Courier, monospace;
+        font-size: 95%;
+        line-height: 140%;
+        white-space: pre;
+        white-space: pre-wrap;
+        white-space: -moz-pre-wrap;
+        white-space: -o-pre-wrap;
+}
+
+p {
+    font-family: "Times New Roman", Times, serif;
+}
+</style>
+
+
+</head>
+<body>
+
+
+	<h1>Drupal Audit : $ENVIRONMENT : $TODAY</h1>
+
+	<img src='https://s3.amazonaws.com/public-jacob/misc/drupal_logo.png' alt='Drupal Logo' height="50%" >
+
+	<p>by: $USER </p>
+
+" > $REPORT  
+
+
+
+####
+echo "<h2>Status Report</h2>" >> $REPORT
+echo "<p>
+
+# core-requirements     Provides information about things that may be wrong in your Drupal installation, if any.
+# (status-report, rq)
+
+</p>" >> $REPORT
+echo "<pre>" >> $REPORT
+
+	drush $DRUSH_ALIAS status-report | tee -a $REPORT
+
+echo "</pre>" >> $REPORT
+###
+
+###
+echo "<h2>UpdateDB Status</h2>" >> $REPORT
+echo "<p>
+
+# updatedb-status       List any pending database updates.
+
+</p>" >> $REPORT
+echo "<pre>" >> $REPORT
+
+	drush $DRUSH_ALIAS updatedb-status | tee -a $REPORT
+
+echo "</pre>" >> $REPORT
+###
+
+###
+echo "<h2>Locale Check</h2>" >> $REPORT
+echo "<p>
+
+# locale-check          Checks for available translation updates.
+
+</p>" >> $REPORT
+echo "<pre>" >> $REPORT
+
+	drush $DRUSH_ALIAS locale-check | tee -a $REPORT
+
+echo "</pre>" >> $REPORT
+###
+
+###
+echo "<h2>PM Update Status</h2>" >> $REPORT
+echo "<p>
+
+# pm-updatestatus       Show a report of available minor updates to Drupal core and contrib projects.
+ (ups)
+
+
+</p>" >> $REPORT
+echo "<pre>" >> $REPORT
+
+	drush $DRUSH_ALIAS pm-updatestatus | tee -a $REPORT
+
+echo "</pre>" >> $REPORT
+###
+
+###
+echo "<h2>Search Status</h2>" >> $REPORT
+echo "<p>
+
+# search-status         Show how many items remain to be indexed out of the total.
+
+</p>" >> $REPORT
+echo "<pre>" >> $REPORT
+
+	drush $DRUSH_ALIAS search-status | tee -a $REPORT
+
+echo "</pre>" >> $REPORT
+###
+
+###
+echo "<h2>Views Analyze</h2>" >> $REPORT
+echo "<p>
+
+# views-analyze (va)    Get a list of all Views analyze warnings
+
+</p>" >> $REPORT
+echo "<pre>" >> $REPORT
+
+	drush $DRUSH_ALIAS views-analyze | tee -a $REPORT
+
+echo "</pre>" >> $REPORT
+###
+
+###
+echo "<h2>Watchdog List</h2>" >> $REPORT
+echo "<p>
+
+# watchdog-show        Show watchdog messages.
+
+</p>" >> $REPORT
+echo "<pre>" >> $REPORT
+
+	drush $DRUSH_ALIAS watchdog-show | tee -a $REPORT
+
+echo "</pre>" >> $REPORT
+###
+
+###
+echo "<h2>Cache Audit</h2>" >> $REPORT
+echo "<p>
+
+# cacheaudit            Show a report on configured cache settings.
+
+</p>" >> $REPORT
+echo "<pre>" >> $REPORT
+
+	drush $DRUSH_ALIAS cacheaudit | tee -a $REPORT
+
+echo "</pre>" >> $REPORT
+###
+
+
+
+
+# close html
+echo "</body>
+</html>" >> $REPORT
+
+
+# DONE
+echo '
+
+
+		=] 	Report is ready @ $REPORT	'
+
+
+
+
